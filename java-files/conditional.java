ed35ca6a7299e57c460a919144c7b3aa15f14151
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-08-09 11:36:54 +0530

    initial files or basic files

---
+++ java-files/conditional.java
@@ -0,0 +1,14 @@
+class condition{
+    public static void main(String args[]) {
+    int age=15;
+    if(age>18){
+        System.out.println("age is greater than 18");
+    }
+    else if(age<10){
+        System.out.println("age is less than 18");
+    }
+    else{
+        System.out.println("age is b/w 10 to 18");
+    }
+    }
+}
---
+++ java-files/forloops.java
@@ -0,0 +1,39 @@
+class forloop {
+    public static void main(String args[]) {
+        //int n=1;
+        // while(i<=5){
+        //     System.out.println("Hi"+i);
+        //     i++;
+        // }
+        // int n=3;
+        // switch (n) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid Number");
+        //         break;
+        // }
+        for(int i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }
+}
