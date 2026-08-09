a3160be79894b44dd72aa766c5d535771d8dc9ad
muralivmuraliv89@gmail.com <muralivmuraliv89@gmail.com>
2026-08-09 11:32:08 +0530

    Datatypes and more

---
+++ java-files/sec.java
@@ -0,0 +1,34 @@
+class seco{
+    public static void main(String args[]) {
+        // int h = 12;        
+          
+        // float h1 = h;
+        // byte h2 = (byte) h;
+        // System.out.println(h);
+        // System.out.println(h1);
+        // System.out.println(h2);
+
+        // float f = 1255f;      
+        // System.out.println(f);
+
+        // char c='m';
+        // System.out.println(c);
+
+        // boolean b = true;
+        // System.out.println(b);
+
+        // String name = "Murali.v";
+        // System.out.println(name);
+
+        // Mathematical operations 
+        int m1=5;
+        int m2=5;
+        System.out.println(m1+m2);
+
+        // Increment and decrement. 
+        int res=m1++;//post Increment.fetch Value and increment 
+        System.out.println(res);
+        res=++m2;//Pre Increment.Increment and then fetch the value. 
+        System.out.println(res);
+    }
+}
